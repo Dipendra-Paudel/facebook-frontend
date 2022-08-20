179af8ff31397741af8315171d9f6a6f5f158110
Dipendra-Paudel <dipebro006@gmail.com>
2022-08-20 19:47:38 +0545

    click event added

--- components/ui/navigation/left/LeftNavigation.jsx
+++ components/ui/navigation/left/LeftNavigation.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import Image from "next/image";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import Search from "./Search";
 import RecentSearches from "./RecentSearches";
 
 const LeftNavigation = () => {
+  const leftContainer = useRef();
   const [isSearchFocused, setIsSearchFocused] = useState(false);
   const [searchText, setSearchText] = useState("");
 
+  useEffect(() => {
+    const handleClick = (event) => {};
+
+    window.addEventListener("click", handleClick);
+  }, []);
+
   return (
-    <div className="px-4 py-2 relative w-80">
+    <div className="px-4 py-2 relative w-80" ref={leftContainer}>
       <div className="flex space-x-2">
         <div className="flex-shrink-0 h-10 w-10 relative">
           <Image src="/images/facebook.png" layout="fill" />
@@ -26,7 +33,7 @@ const LeftNavigation = () => {
 
       <div
         className={`absolute top-0 w-full left-0 py-2 bg-white transition-all duration-300 ${
-          isSearchFocused ? "shadow z-10" : "-z-10"
+          isSearchFocused ? "shadow-xl z-10 rounded-md" : "-z-10"
         }`}
       >
         <div
@@ -37,7 +44,7 @@ const LeftNavigation = () => {
           <KeyboardBackspaceIcon className="text-gray-700" />
         </div>
 
-        <RecentSearches />
+        {isSearchFocused && <RecentSearches />}
       </div>
     </div>
   );
--- components/ui/navigation/left/RecentSearches.jsx
+++ components/ui/navigation/left/RecentSearches.jsx
@@ -1,27 +1,75 @@
 import { useEffect, useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
 
 const dummySearchHistory = [
   {
     text: "Jpt",
   },
   {
-    text: "Jpt",
+    text: "Test",
   },
   {
-    text: "Jpt",
+    text: "Hello",
   },
 ];
 
 const RecentSearches = () => {
   const [searchHistory, setSearchHistory] = useState([]);
+  const [fetching, setFetching] = useState(true);
+  const [mounted, setMounted] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => {}, 3000);
+    setTimeout(() => {
+      if (!mounted) return false;
+
+      setSearchHistory(dummySearchHistory);
+      setFetching(false);
+    }, 500);
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      setMounted(false);
+    };
   }, []);
 
   return (
-    <div>
-      <div>Recent Searches</div>
+    <div className="p-2 pb-0 pt-5">
+      {!fetching && (
+        <div>
+          <div className="flex justify-between items-center pb-2">
+            <div className="font-bold text-gray-700 pl-2">Recent searches</div>
+            <div className="text-sm text-blue-500">Edit</div>
+          </div>
+
+          <div>
+            {searchHistory.map((history, index) => {
+              const { text } = history;
+              return (
+                <div
+                  key={index}
+                  className="flex justify-between items-center hover:bg-gray-200 p-2 cursor-pointer rounded-lg"
+                >
+                  <div className="flex items-center space-x-2">
+                    <div className="bg-gray-100 h-8 w-8 flex items-center justify-center rounded-full text-gray-600">
+                      <AccessTimeIcon style={{ fontSize: "20px" }} />
+                    </div>
+                    <div className="text-sm text-gray-700">{text}</div>
+                  </div>
+
+                  <div className="h-7 w-7 flex items-center justify-center rounded-full hover:bg-gray-300">
+                    <CloseIcon
+                      className="text-gray-600"
+                      style={{ fontSize: "18px" }}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
